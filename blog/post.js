c90ba963ade2957ed6a3949a7ee925a49e497fb4
Pixels67 <pixels67@proton.me>
2026-03-17 13:52:31 +0300

    Update website to use blogserve backend

--- blog/post.js
+++ blog/post.js
@@ -1,23 +1,20 @@
 const slug = new URLSearchParams(window.location.search).get('slug');
-const post = POSTS.find(p => p.slug === slug);
-
-if (!post) {
-    document.getElementById('post').innerHTML = '<h2>Post not found.</h2>';
-} else {
-    document.title = post.title;
-
-    marked.use({
-        renderer: {
-            code(code, lang) {
-                const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-                return `<pre><code class="hljs language-${language}">${hljs.highlight(code, {language}).value}</code></pre>`;
+fetch('https://blogserve-kltj.onrender.com/posts/' + slug)
+    .then(response => response.json())
+    .then(post => {
+        document.title = post.title;
+        marked.use({
+            renderer: {
+                code(code, lang) {
+                    const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+                    return `<pre><code class="hljs language-${language}">${hljs.highlight(code, {language}).value}</code></pre>`;
+                }
             }
-        }
-    });
-
-    fetch(post.file)
-        .then(res => res.text())
-        .then(md => {
-            document.getElementById('post').innerHTML = `${marked.parse(md)}`;
         });
-}
+
+        document.getElementById('post').innerHTML = marked.parse(post.content);
+    })
+    .catch(error => {
+        console.error('Error:', error)
+        document.getElementById('post').innerHTML = `<h2>Error retrieving post from server!</h2>`;
+    });
--- blog/posts.js
+++
@@ -1,8 +0,0 @@
-const POSTS = [
-    {
-        title: 'Flock devlog #1',
-        date: '2026-03-12',
-        file: 'posts/post1.md',
-        slug: 'flock-devlog1'
-    }
-];
